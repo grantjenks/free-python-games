36e88e58e5963d2fa9d0098d25703f932c940597
Grant Jenks <grant.jenks@gmail.com>
2012-05-03 13:23:04 -0400

    Finish up pacman game.

--- pacman.py
+++ pacman.py
@@ -1,5 +1,5 @@
 """
-Pacman.
+Simplified Pacman.
 
 Written by Grant Jenks
 http://www.grantjenks.com/
@@ -23,182 +23,185 @@ or send a letter to Creative Commons, 171 Second Street, Suite 300,
 San Francisco, California, 94105, USA
 """
 
-"""
-Needs
-- 4 ghosts
-- end game scenario:
-  * everything eaten
-  * eaten by ghost
-"""
-
 import sys
-import math
 import pygame
 import random
 from pygame.locals import *
-from itertools import count
 
 pygame.init()
 
-board_width, board_height = 17, 19
 font = pygame.font.Font(None, 24)
-pacman = pygame.image.load('pacman.jpg')
-ghost = pygame.image.load('ghost.jpg')
+pacman_img = pygame.image.load('pacman.jpg')
+ghost_img = pygame.image.load('ghost.jpg')
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((340, 400))
 
-score = 0
-direction = new_dir = 0
-board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-         0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0,
-         0, 2, 0, 0, 2, 0, 0, 2, 0, 2, 0, 0, 2, 0, 0, 2, 0,
-         0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0,
-         0, 2, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 2, 0,
-         0, 2, 2, 2, 2, 0, 2, 2, 0, 2, 2, 0, 2, 2, 2, 2, 0,
-         0, 2, 0, 0, 2, 0, 0, 2, 0, 2, 0, 0, 2, 0, 0, 0, 0,
-         0, 2, 0, 0, 2, 0, 2, 2, 2, 2, 2, 0, 2, 0, 0, 0, 0,
-         0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 2, 2, 2, 2, 2, 2, 0,
-         0, 0, 0, 0, 2, 0, 2, 2, 2, 2, 2, 0, 2, 0, 0, 2, 0,
-         0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 2, 0,
-         0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0,
-         0, 2, 0, 0, 2, 0, 0, 2, 0, 2, 0, 0, 2, 0, 0, 2, 0,
-         0, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 0,
-         0, 0, 2, 0, 2, 0, 2, 0, 0, 0, 2, 0, 2, 0, 2, 0, 0,
-         0, 2, 2, 2, 2, 0, 2, 2, 0, 2, 2, 0, 2, 2, 2, 2, 0,
-         0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0,
-         0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0,
-         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-pac_pos_lft = 160
-pac_pos_top = 260
-ghosts = [[20, 20, 0],
-          [300, 20, 2],
-          [20, 340, 0],
-          [300, 340, 2]]
+class Game:
+    """Track pacman game state."""
+    def __init__(self):
+        """Initialize game."""
+        self.reset()
 
-def update_position(new_dir, lft, top):
-    new_lft = lft
-    new_top = top
+    def reset(self):
+        """Reset game to initial game state."""
+        self.running = True
+        self.score = 0
 
-    if new_dir == 0:
-        new_lft += 4
-    elif new_dir == 1:
-        new_top -= 4
-    elif new_dir == 2:
-        new_lft -= 4
-    elif new_dir == 3:
-        new_top += 4
+        self.tiles_width = 17
+        self.tiles = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0,
+                      0, 2, 0, 0, 2, 0, 0, 2, 0, 2, 0, 0, 2, 0, 0, 2, 0,
+                      0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0,
+                      0, 2, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 2, 0,
+                      0, 2, 2, 2, 2, 0, 2, 2, 0, 2, 2, 0, 2, 2, 2, 2, 0,
+                      0, 2, 0, 0, 2, 0, 0, 2, 0, 2, 0, 0, 2, 0, 0, 0, 0,
+                      0, 2, 0, 0, 2, 0, 2, 2, 2, 2, 2, 0, 2, 0, 0, 0, 0,
+                      0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 2, 2, 2, 2, 2, 2, 0,
+                      0, 0, 0, 0, 2, 0, 2, 2, 2, 2, 2, 0, 2, 0, 0, 2, 0,
+                      0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 2, 0,
+                      0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0,
+                      0, 2, 0, 0, 2, 0, 0, 2, 0, 2, 0, 0, 2, 0, 0, 2, 0,
+                      0, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 0,
+                      0, 0, 2, 0, 2, 0, 2, 0, 0, 0, 2, 0, 2, 0, 2, 0, 0,
+                      0, 2, 2, 2, 2, 0, 2, 2, 0, 2, 2, 0, 2, 2, 2, 2, 0,
+                      0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0,
+                      0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-    return new_lft, new_top
+        self.next_dir = 0
+        self.pacman = [pygame.Rect(160, 260, 20, 20), 0]
+        self.ghosts = [[pygame.Rect(20, 20, 20, 20), 0],
+                       [pygame.Rect(300, 20, 20, 20), 2],
+                       [pygame.Rect(20, 340, 20, 20), 0],
+                       [pygame.Rect(300, 340, 20, 20), 2]]
 
-def calculate_idx(lft, top):
-    return (top / 20) * board_width + (lft / 20)
+    def next_pos(self, next_dir, rect):
+        """Advance rectangle by given direction."""
+        if next_dir == 0: chng = (4, 0)
+        if next_dir == 1: chng = (0, -4)
+        if next_dir == 2: chng = (-4, 0)
+        if next_dir == 3: chng = (0, 4)
+        return rect.move(*chng)
 
-def valid_position(lft, top):
-    prev_idx = calculate_idx(lft, top)
-    next_idx = calculate_idx(lft + 19, top + 19)
-    on_track = lft % 20 == 0 or top % 20 == 0
-    return board[prev_idx] != 0 and board[next_idx] != 0 and on_track
+    def rect_idx(self, rect):
+        """Calculate tile index by rectangle."""
+        return (rect.top / 20) * self.tiles_width + (rect.left / 20)
 
-def update_ghost(val):
-    cur_dir = new_dir = val[2]
-    if val[0] % 20 == 0 and val[1] % 20 == 0:
-        new_dir = (cur_dir + random.choice((1, 3))) % 4
-    new_lft, new_top = update_position(new_dir, val[0], val[1])
-    if valid_position(new_lft, new_top):
-        val[0] = new_lft
-        val[1] = new_top
-        val[2] = new_dir
-    else:
-        new_lft, new_top = update_position(cur_dir, val[0], val[1])
-        if valid_position(new_lft, new_top):
-            val[0] = new_lft
-            val[1] = new_top
+    def valid_pos(self, rect):
+        """Return True iff character rectangle is valid on tiles."""
+        prev_tile = self.tiles[self.rect_idx(rect)]
+        next_tile = self.tiles[self.rect_idx(rect.move(19, 19))]
+        on_track = rect.left % 20 == 0 or rect.top % 20 == 0
+        return prev_tile != 0 and next_tile != 0 and on_track
 
-for counter in count():
+    def move_rect(self, next_dir, curr_dir, rect):
+        """
+        Move character rectangle.
+        Returns next character location and direction.
+        """
+        next_rect = self.next_pos(next_dir, rect)
+        if self.valid_pos(next_rect):
+            return (next_rect, next_dir)
+        else:
+            next_rect = self.next_pos(curr_dir, rect)
+            if self.valid_pos(next_rect):
+                return (next_rect, curr_dir)
+        return (rect, curr_dir)
+
+    def move_pacman(self):
+        """Move pacman character."""
+        rect, curr_dir = self.pacman
+        tup = self.move_rect(self.next_dir, curr_dir, rect)
+        self.pacman[0], self.pacman[1] = tup
+
+    def move_ghosts(self):
+        """Move ghost characters."""
+        for tup in self.ghosts:
+            rect = tup[0]
+            curr_dir = next_dir = tup[1]
+            if rect.top % 20 == 0 and rect.left % 20 == 0:
+                next_dir = (curr_dir + random.choice((1, 3))) % 4
+            tup[0], tup[1] = self.move_rect(next_dir, curr_dir, rect)
+
+    def update(self):
+        """Update tiles, score and running based on pacman location."""
+        self.next_dir = self.pacman[1]
+
+        idx = self.rect_idx(self.pacman[0])
+        if self.tiles[idx] == 2:
+            self.score += 1
+            self.tiles[idx] = 1
+
+        has_food = any(val == 2 for val in self.tiles)
+        ghost_rects = [tup[0] for tup in self.ghosts]
+        is_eaten = self.pacman[0].collidelist(ghost_rects) >= 0
+        self.running = has_food and not is_eaten
+
+    def draw(self):
+        """Draw game on screen."""
+        # Draw tiles
+
+        for idx, val in enumerate(self.tiles):
+            left = (idx % self.tiles_width) * 20
+            top = (idx / self.tiles_width) * 20
+            if val == 0:
+                color = (0, 0, 128)
+                pygame.draw.rect(screen, color, (left, top, 20, 20))
+            else:
+                color = (0, 0, 0)
+                pygame.draw.rect(screen, color, (left, top, 20, 20))
+            if val == 2:
+                color = (255, 255, 255)
+                pygame.draw.circle(screen, color, (left + 10, top + 10), 2)
+
+        # Draw pacman.
+
+        pacman_dir = pygame.transform.rotate(pacman_img, 90 * self.pacman[1])
+        screen.blit(pacman_dir, self.pacman[0])
+
+        # Draw ghosts.
+
+        for val in self.ghosts:
+            screen.blit(ghost_img, val[0])
+
+        # Draw score.
+
+        pygame.draw.rect(screen, (0, 0, 0), (0, 380, 100, 20))
+        msg = 'Score: ' + str(self.score)
+        text = font.render(msg , True, (255, 255, 255))
+        screen.blit(text, (5, 382))
+
+game = Game()
+
+while True:
 
     event = pygame.event.poll()
 
-    # Response to key presses.
+    # Respond to key presses.
 
     if event.type == pygame.QUIT:
         pygame.quit()
         sys.exit()
     elif event.type == KEYDOWN:
         if event.key == K_UP:
-            new_dir = 1
+            game.next_dir = 1
         elif event.key == K_RIGHT:
-            new_dir = 0
+            game.next_dir = 0
         elif event.key == K_DOWN:
-            new_dir = 3
+            game.next_dir = 3
         elif event.key == K_LEFT:
-            new_dir = 2
+            game.next_dir = 2
         elif event.key == K_r:
-            pass
+            game.reset()
         elif event.key == K_q:
             pygame.event.post(pygame.event.Event(QUIT))
 
-    # Update pacman position.
+    if game.running:
+        game.move_pacman()
+        game.move_ghosts()
+        game.update()
 
-    new_lft, new_top = update_position(new_dir, pac_pos_lft, pac_pos_top)
-
-    if valid_position(new_lft, new_top):
-        pac_pos_lft = new_lft
-        pac_pos_top = new_top
-        direction = new_dir
-    else:
-        new_lft, new_top = update_position(direction, pac_pos_lft, pac_pos_top)
-
-        if valid_position(new_lft, new_top):
-            pac_pos_lft = new_lft
-            pac_pos_top = new_top
-
-    new_dir = direction
-
-    # Update ghosts position.
-
-    for val in ghosts:
-        update_ghost(val)
-
-    # Update board.
-
-    idx = calculate_idx(pac_pos_lft, pac_pos_top)
-    if board[idx] == 2:
-        score += 1
-        board[idx] = 1
-
-    # Draw the board.
-
-    for idx, val in enumerate(board):
-        left = (idx % board_width) * 20
-        top = (idx / board_width) * 20
-        if val == 0:
-            color = (0, 0, 128)
-            pygame.draw.rect(screen, color, (left, top, 20, 20))
-        else:
-            color = (0, 0, 0)
-            pygame.draw.rect(screen, color, (left, top, 20, 20))
-            if val == 2:
-                color = (255, 255, 255)
-                pygame.draw.circle(screen, color, (left + 10, top + 10), 2)
-
-    # Draw pacman.
-
-    pacman_dir = pygame.transform.rotate(pacman, 90 * direction)
-    screen.blit(pacman_dir, (pac_pos_lft, pac_pos_top))
-
-    # Draw ghosts.
-
-    for val in ghosts:
-        screen.blit(ghost, (val[0], val[1]))
-
-    # Draw score.
-
-    pygame.draw.rect(screen, (0, 0, 0), (0, 380, 100, 20))
-    msg = 'Score: ' + str(score)
-    text = font.render(msg , True, (255, 255, 255))
-    screen.blit(text, (5, 382))
-
-    # TODO Check end game condition.
+    game.draw()
 
     # Display next frame.
 
