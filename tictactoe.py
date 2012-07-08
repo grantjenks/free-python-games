59299e0d8da8f517a05bddb8a0dff67a4469fc86
Grant Jenks <grant.jenks@gmail.com>
2012-07-08 14:03:45 -0400

    Tic Tac Toe game.

---
+++ tictactoe.py
@@ -0,0 +1,166 @@
+"""
+Tic Tac Toe.
+
+Written by Grant Jenks
+http://www.grantjenks.com/
+
+DISCLAIMER
+THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE
+LAW. EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR
+OTHER PARTIES PROVIDE THE PROGRAM "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU.
+SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY
+SERVICING, REPAIR OR CORRECTION.
+
+Copyright
+This work is licensed under the
+Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+To view a copy of this license, visit
+http://creativecommons.org/licenses/by-nc-nd/3.0/
+or send a letter to Creative Commons, 171 Second Street, Suite 300,
+San Francisco, California, 94105, USA
+"""
+
+import sys, pygame, random
+from pygame.locals import *
+
+pygame.init()
+
+size = width, height = 480, 480
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+
+black, red, blue, white = (0, 0, 0), (255, 0, 0), (0, 0, 255), (255, 255, 255)
+lose, tie, win = xxx, empty, ooo = -1, 0, 1
+human = ooo
+
+def draw_game():
+    pygame.draw.rect(screen, white, (0, 0, width, height))
+
+    pygame.draw.line(screen, black, (160, 0), (160, 480), 5)
+    pygame.draw.line(screen, black, (320, 0), (320, 480), 5)
+    pygame.draw.line(screen, black, (0, 160), (480, 160), 5)
+    pygame.draw.line(screen, black, (0, 320), (480, 320), 5)
+
+    for row, line in enumerate(board):
+        for col, val in enumerate(line):
+            if val == xxx:
+                # Draw an X as two crossing lines.
+
+                upper_left = (col * 160 + 5, row * 160 + 5)
+                lower_right = (col * 160 + 155, row * 160 + 155)
+                pygame.draw.line(screen, red, upper_left, lower_right, 5)
+
+                upper_right = (col * 160 + 155, row * 160 + 5)
+                lower_left = (col * 160 + 5, row * 160 + 155)
+                pygame.draw.line(screen, red, upper_right, lower_left, 5)
+            elif val == ooo:
+                rect = (col * 160 + 5, row * 160 + 5, 150, 150)
+                pygame.draw.ellipse(screen, blue, rect, 5)
+            else:
+                assert val == empty
+                continue
+
+def is_won():
+    for val in range(3):
+        # Check matching row.
+
+        if board[0][val] == board[1][val] == board[2][val]:
+            return board[0][val]
+
+        # Check matching column.
+
+        if board[val][0] == board[val][1] == board[val][2]:
+            return board[val][0]
+
+    # Check matching diagonal.
+
+    if board[0][0] == board[1][1] == board[2][2]:
+        return board[1][1]
+
+    if board[0][2] == board[1][1] == board[2][0]:
+        return board[1][1]
+
+    return empty
+
+def calc_move(player):
+    if all(val == empty for line in board for val in line):
+        row = random.randrange(3)
+        col = random.randrange(3)
+        result = (tie, (row, col))
+    else:
+        result = best_move(player)
+    return result
+
+def best_move(player):
+    """
+    Return (outcome, (row, col)) indicating this player's best move.
+    """
+    if is_won() == player * -1: return (lose, None)
+
+    best = None
+    for row, line in enumerate(board):
+        for col, val in enumerate(line):
+            if val == empty:
+                # Simulate this move.
+
+                board[row][col] = player
+
+                # Compute the best counter-move result.
+
+                counter = best_move(player * -1)
+
+                # Update our best move based on the counter-move result.
+
+                if best is None or best[0] <= (counter[0] * -1):
+                    best = (counter[0] * -1, (row, col))
+
+                # Undo simulated move.
+
+                board[row][col] = empty
+
+    if best is None: best = (tie, None)
+
+    return best
+
+def restart():
+    global board, turn, human
+    human *= -1
+    board = [[empty] * 3, [empty] * 3, [empty] * 3]
+    turn = xxx
+    draw_game()
+    pygame.display.flip()
+
+restart()
+
+while True:
+    event = pygame.event.poll()
+
+    pos = None
+
+    if event.type == pygame.QUIT:
+        pygame.quit()
+        sys.exit()
+    elif event.type == KEYDOWN:
+        if event.key == K_r:
+            restart()
+            draw_game()
+            pygame.display.flip()
+        elif event.key == K_q:
+            pygame.event.post(pygame.event.Event(QUIT))
+    elif event.type == MOUSEBUTTONDOWN and event.button == 1:
+        pos = (event.pos[1] / 160, event.pos[0] / 160)
+
+    if human != turn:
+        pos = calc_move(turn)[1]
+
+    if pos is not None and not is_won():
+        row, col = pos
+        board[row][col] = turn
+        turn *= -1
+        draw_game()
+
+    pygame.display.flip()
+    clock.tick(12)
