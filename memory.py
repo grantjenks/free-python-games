ccbdc38d6ff56a67d65537dd0ed8125599646ff5
Grant Jenks <grant.jenks@gmail.com>
2012-08-09 13:46:59 -0700

    Finishing touches on memory game. Code complete and tested.

--- memory.py
+++ memory.py
@@ -37,37 +37,55 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode(size)
 image = pygame.image.load(sys.argv[1])
 font = pygame.font.Font(None, 60)
+black, white = (0, 0, 0), (255, 255, 255)
 
 # Create tiles.
 
 tiles = []
-for y_pos in range(height / tile_size):
-    for x_pos in range(width / tile_size):
-        area = pygame.Rect(x_pos * tile_size, y_pos * tile_size,
-                           tile_size, tile_size)
+for y_pos in range(0, height, tile_size):
+    for x_pos in range(0, width, tile_size):
+        area = pygame.Rect(x_pos, y_pos, tile_size, tile_size)
         tile = pygame.Surface((tile_size, tile_size))
         tile.blit(image, (0, 0), area)
         tiles.append((area, tile))
 
 def draw_game():
-    pygame.draw.rect(screen, (0, 0, 0), (0, 0, width, height))
+    """Draw the game state."""
+
+    # Clear the screen with a black background.
+
+    pygame.draw.rect(screen, black, (0, 0, width, height))
+
+    # Draw lines to create grid for tiles.
+
     for pos in range(1, width / tile_size):
         offset = pos * tile_size
-        pygame.draw.line(screen, (255, 255, 255), (offset, 0), (offset, height))
+        pygame.draw.line(screen, white, (offset, 0), (offset, height))
+
     for pos in range(1, height / tile_size):
         offset = pos * tile_size
-        pygame.draw.line(screen, (255, 255, 255), (0, offset), (width, offset))
+        pygame.draw.line(screen, white, (0, offset), (width, offset))
+
+    # Draw tiles correctly guessed.
+
     for pos, tile in enumerate(tiles):
         if guesses[pos]:
             screen.blit(tile[1], tile[0])
-    if select is not None:
-        surface = font.render(str(values[select]), True, (255, 255, 255))
-        screen.blit(surface, tiles[select][0])
+
+    def draw_number(value):
+        """Draw the number at a particular tile."""
+        surface = font.render(str(values[value]), True, white)
+        screen.blit(surface, tiles[value][0].move(45, 45))
+
+    if select is not None: draw_number(select)
+    if match is not None: draw_number(match)
+
     pygame.display.flip()
 
 def restart():
-    global values, guesses, select
-    select = None
+    """Reset the game with random values."""
+    global values, guesses, select, match, pause
+    select, match, pause = None, None, 0
     count = len(tiles)
     assert count % 2 == 0
     values = list(range(count / 2)) * 2
@@ -79,8 +97,10 @@ restart()
 
 while True:
     clock.tick(12)
+
     event = pygame.event.poll()
     pos = None
+
     if event.type == pygame.QUIT:
         pygame.quit()
         sys.exit()
@@ -92,11 +112,24 @@ while True:
     elif event.type == MOUSEBUTTONDOWN and event.button == 1:
         pos = event.pos
 
+    if pause > 0:
+        if pos is None:
+            if pause == 1:
+                select, match, pause = None, None, 0
+                draw_game()
+            else:
+                pause -= 1
+                continue
+        else:
+            select, match, pause = None, None, 0
+            draw_game()
+
     if pos is None: continue
 
     xxx = pos[0] / tile_size
     yyy = pos[1] / tile_size
     index = yyy * (width / tile_size) + xxx
+
     if guesses[index]:
         continue
     else:
@@ -111,5 +144,7 @@ while True:
                     guesses[index] = True
                     select = None
                 else:
-                    select = None
+                    pause = 12 * 3
+                    match = index
+
     draw_game()
