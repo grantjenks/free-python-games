dcb46aab4c8b8a3437df4b233db272d09d7f0637
Grant Jenks <contact@grantjenks.com>
2014-07-20 19:12:09 -0700

    Update copyright notice

--- crypto.py
+++ crypto.py
@@ -1,31 +1,11 @@
 """
 Crypto, a tool for encrypting, decrypting and decoding messages.
 
-Written by Grant Jenks
+Copyright (c) 2014 Grant Jenks
 http://www.grantjenks.com/
 
 Based on the Caesar Cipher algorithm from:
 http://inventwithpython.com/chapter14.html
-
-Copyright (c) 2014 Grant Jenks
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to
-deal in the Software without restriction, including without limitation the
-rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-sell copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-IN THE SOFTWARE.
 
 Exercises
 0. Review 'ord' and 'chr' functions and letter-to-number mapping.
