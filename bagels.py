70d170fe1b7c4ffb8929d27e74a5f400f1863d26
Grant Jenks <contact@grantjenks.com>
2014-07-21 10:07:33 -0700

    Re-write bagels

--- bagels.py
+++ bagels.py
@@ -1,83 +1,86 @@
 """
-Bagels.
+Bagels: a number game.
 
-Copied from: http://inventwithpython.com/chapter11.html
+Copyright (c) 2014 Grant Jenks
+http://www.grantjenks.com/
+
+Based on the Bagels game from:
+http://inventwithpython.com/chapter11.html
+
+Exercises:
+1. Can you guess the number?
+2. How much harder is 6 digits? Do you need more guesses?
+3. What's the maximum number of digits we could support?
 """
 
-import random
+from random import sample, shuffle
 
-def getSecretNum(numDigits):
-    # Returns a string that is numDigits long, made up of unique random digits.
-    numbers = list(range(10))
-    random.shuffle(numbers)
-    secretNum = ''
-    for i in range(numDigits):
-        secretNum += str(numbers[i])
-    return secretNum
+digits = 3
+guesses = 10
 
-def getClues(guess, secretNum):
-    # Returns a string with the pico, fermi, bagels clues to the user.
-    if guess == secretNum:
-        return 'You got it!'
-
-    clue = []
-
-    for i in range(len(guess)):
-        if guess[i] == secretNum[i]:
-            clue.append('Fermi')
-        elif guess[i] in secretNum:
-            clue.append('Pico')
-    if len(clue) == 0:
-        return 'Bagels'
-
-    clue.sort()
-    return ' '.join(clue)
-
-def isOnlyDigits(num):
-    # Returns True if num is a string made up only of digits. Otherwise returns False.
-    if num == '':
-        return False
-
-    for i in num:
-        if i not in '0 1 2 3 4 5 6 7 8 9'.split():
-            return False
-
-    return True
-
-def playAgain():
-    # This function returns True if the player wants to play again, otherwise it returns False.
-    print 'Do you want to play again? (yes or no)'
-    return raw_input().lower().startswith('y')
-
-NUMDIGITS = 3
-MAXGUESS = 10
-
-print 'I am thinking of a %s-digit number. Try to guess what it is.' % (NUMDIGITS)
+print 'I am thinking of a {}-digit number. Try to guess what it is.'.format(digits)
 print 'Here are some clues:'
 print 'When I say:    That means:'
 print '  Pico         One digit is correct but in the wrong position.'
 print '  Fermi        One digit is correct and in the right position.'
 print '  Bagels       No digit is correct.'
+print 'There are no repeated digits in the number.'
 
 while True:
-    secretNum = getSecretNum(NUMDIGITS)
-    print 'I have thought up a number. You have %s guesses to get it.' % (MAXGUESS)
+    # Create a random number.
 
-    numGuesses = 1
-    while numGuesses <= MAXGUESS:
-        guess = ''
-        while len(guess) != NUMDIGITS or not isOnlyDigits(guess):
-            print 'Guess #%s: ' % (numGuesses)
-            guess = raw_input()
+    letters = sample('0123456789', digits)
 
-        clue = getClues(guess, secretNum)
-        print clue
-        numGuesses += 1
+    if letters[0] == '0':
+        letters = letters[::-1]
 
-        if guess == secretNum:
+    number = ''.join(letters)
+
+    print 'I have thought up a number. You have {} guesses to get it.'.format(guesses)
+
+    counter = 1
+
+    while True:
+        prompt = 'Guess #{}: '.format(counter)
+        guess = raw_input(prompt)
+
+        if len(guess) != digits:
+            print 'Wrong number of digits. Try again!'
+            continue
+
+        if not all(letter in '0123456789' for letter in guess):
+            print 'That is not a number. Try again!'
+            continue
+
+        # Create the clues.
+
+        clues = []
+
+        for pos in range(len(guess)):
+
+            if guess[pos] == number[pos]:
+                clues.append('Fermi')
+            elif guess[pos] in number:
+                clues.append('Pico')
+
+        shuffle(clues)
+
+        if len(clues) == 0:
+            print 'Bagels'
+        else:
+            print ' '.join(clues)
+
+        counter += 1
+
+        if guess == number:
+            print 'You got it!'
             break
-        if numGuesses > MAXGUESS:
-            print 'You ran out of guesses. The answer was %s.' % (secretNum)
 
-    if not playAgain():
+        if counter > guesses:
+            print 'You ran out of guesses. The answer was {}.'.format(number)
+            break
+
+    print 'Do you want to play again? (yes or no)'
+    answer = raw_input()
+    if not answer.lower().startswith('y'):
         break
