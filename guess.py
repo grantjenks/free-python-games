d52f883531374d873d385ef0430ff902a33fe24d
Grant Jenks <contact@grantjenks.com>
2014-07-20 23:04:59 -0700

    Update guess.py exercises

--- guess.py
+++ guess.py
@@ -7,7 +7,8 @@ http://www.grantjenks.com/
 Exercises
 1. Change the range to be from 0 to 1,000,000.
 2. Can you still guess the number?
-3. Print out the number of required guesses for the right answer.
+3. Print the number of guesses made.
+4. Limit the number of guesses to the minimum required.
 """
 
 import random
