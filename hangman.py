12741045e16891fd628093828deec72cec747a5f
NolanDriessen <driessnp@mcmaster.ca>
2017-03-07 16:16:40 -0500

    Added code documentation for hangman.py, maze.py and pong.py.

--- hangman.py
+++ hangman.py
@@ -15,6 +15,9 @@ import os
 import sys
 
 def draw_man(guess_count):
+    """This is called after each guess by the player.
+    It represents the amount of guesses the player has left in the spirit of hangman.
+    """
     if guess_count == 0:
         print ''
         print ''
@@ -115,59 +118,69 @@ def draw_man(guess_count):
         print '  |'
         print '-----'
 
+"""Get the word the user is trying to guess as input from user"""
 print 'Enter a word or phrase to guess:'
 answer = sys.stdin.readline()[:-1]
 answer = list(answer)
 
-os.system('cls' if os.name=='nt' else 'clear')
+os.system('cls' if os.name=='nt' else 'clear')  # Clear the command window the game is running in
 
+"""Initalize starting variables
+Wrong guesses is a counter used to end the game at 10
+guess displays the number of letter in the answer for visualization purposes
+guessed_letters keeps track of which letters the user has guessed
+"""
 wrong_guesses = 0
 guess = [' ' if char == ' ' else '_' for char in answer]
 guessed_letters = []
 
 draw_man(wrong_guesses)
-print ''.join(guess)
+print ''.join(guess)  # Print the contents of the guess array, omitting all formatting
 
 while True:
-
+    """Main loop of the program.
+    Displays to the user the letters they have guessed and requests a new guess.
+    Once input is given the screen is cleared and the input is checked to be consistent with expected values.
+    The user is either asked to input a different value or the game is updated based on whether their guess is correct or incorrect
+    """
     guessed_letters.sort()
-    print 'Guessed letters:', ' '.join(guessed_letters)
+    print 'Guessed letters:', ' '.join(guessed_letters)  # Display the letters the user has guessed, sorted alphabetically
     print 'Guess a letter:'
-    letter = sys.stdin.readline()[:-1]
+    letter = sys.stdin.readline()[:-1]  # Ask the user for a new guess
 
-    os.system('cls' if os.name=='nt' else 'clear')
+    os.system('cls' if os.name=='nt' else 'clear')  # Clear the screen
 
-    if len(letter) != 1:
+    if len(letter) != 1:  # If more or less than 1 character was entered
 
         print 'Please enter a single letter.'
 
-    elif letter in guessed_letters:
+    elif letter in guessed_letters:  # If the letter was already guessed
 
         print 'You already guessed that letter, try again.'
 
-    elif letter in answer:
+    elif letter in answer:  # If the letter is correct
 
         print 'Good guess!'
-        for index in range(len(answer)):
-            if answer[index] == letter:
-                guess[index] = letter
-        guessed_letters.append(letter)
+        for index in range(len(answer)):  # Loop through all the letters in the answer
+            if answer[index] == letter:  # If the guess matches the letter at the current index
+                guess[index] = letter  # Update the guess list to include the correct letter
+        guessed_letters.append(letter)  # Add the guessed letter to the guessed_letters list
 
-    else:
+    else:  # If the letter is incorrect
 
         print 'WRONG guess.'
         wrong_guesses += 1
         guessed_letters.append(letter)
 
-    draw_man(wrong_guesses)
-    print ''.join(guess)
+    draw_man(wrong_guesses)  # Draw the man on screen again based on the new amount of wrong guesses
+    print ''.join(guess)  # Display the word as the user has guessed it
 
-    if wrong_guesses == 10:
+    if wrong_guesses == 10:  # End the game if the player has 10 incorrect guesses
 
         print "You're DEAD!"
         break
 
-    if '_' not in guess:
+    if '_' not in guess:  # If the guess array is populated with only letters, the user has completely guessed the answer
 
         print 'Success! You LIVE!'
         break
--- pong.py
+++ pong.py
@@ -11,6 +11,14 @@ Exercises
 import sys, pygame, random
 from pygame.locals import *
 
+"""
+Initalize game start state. Used to setup before the game begins.
+Width and height define the size of the game window
+Clock is used to restrict framerate to the value stored in fps
+lhs and rhs define where the paddles of each player will be placed
+Screen defines a window for the game to be played, calibrated to the width and height parameters
+White and black define RGB triplets for their corresponding colour
+"""
 pygame.init()
 
 lhs, rhs = x, y = 0, 1
@@ -22,68 +30,86 @@ screen = pygame.display.set_mode((width, height))
 white, black = (255, 255, 255), (0, 0, 0)
 
 def draw():
-    pygame.draw.rect(screen, black, (0, 0, width, height))
-    pygame.draw.circle(screen, white, map(int, ball), 2)
-    pygame.draw.rect(screen, white, paddle[lhs])
-    pygame.draw.rect(screen, white, paddle[rhs])
+    """Function to draw each item to the screen. Called each frame."""
+    pygame.draw.rect(screen, black, (0, 0, width, height))  # Draws the entire screen to be black. This overwrites the previous locations of where the ball and paddles used to be.
+    pygame.draw.circle(screen, white, map(int, ball), 2)  # Draws the ball at its current location.
+    pygame.draw.rect(screen, white, paddle[lhs])  # Draws the left paddle at its current location
+    pygame.draw.rect(screen, white, paddle[rhs])  # Draws the right paddle at its current location
 
 def update():
+    """Function to update the position of the ball each frame.
+    If the ball hits the left or right screen boundaries we end the game.
+    If the ball hits the top or bottom the vertical motion is inverted and changed to a random value.
+    If the ball hits a paddle the horizontal movement is inverted and changed to a random value.
+    """
     global dead
 
     ball[x] += motion[x]
     ball[y] += motion[y]
 
-    if ball[x] <= 0 or ball[x] >= width:
+    if ball[x] <= 0 or ball[x] >= width:  # If the ball gets past either paddle, hitting the edge of the screen
         dead = True
         return
 
-    if ball[y] <= 0 or ball[y] >= height:
+    if ball[y] <= 0 or ball[y] >= height:  # If the ball hits the ceiling or floor
         motion[y] *= -1
-        motion[y] += random.random() - 0.5
+        motion[y] += random.random() - 0.5  # Increment the vertical velocity by -0.5 to 0.5 
 
-    ball_rect = Rect(ball[x] - 2, ball[y] - 2, 4, 4)
-    paddles = map(Rect, paddle)
+    ball_rect = Rect(ball[x] - 2, ball[y] - 2, 4, 4)  # Creates a rect around the ball
+    paddles = map(Rect, paddle)  # Creates a rect around each paddle, used for collision detection with ball_rect
 
     if (ball_rect.colliderect(paddles[lhs])
-        or ball_rect.colliderect(paddles[rhs])):
+        or ball_rect.colliderect(paddles[rhs])):  # If the ball hits either paddle
         motion[x] *= -1
-        motion[x] += random.random() - 0.5
+        motion[x] += random.random() - 0.5  # Increment the horizontal velocity by -0.5 to 0.5 
 
 def reset():
+    """Resets the system to the initial state.
+    Dead is set to False to signify the system being active
+    The ball and paddle define the initial locations for their respective objects to be drawn
+    Motion defines a random direction for the ball to move
+    """
     global ball, motion, paddle, dead
     dead = False
     ball = [width / 2, height / 2]
     motion = [random.choice([-7, -6, -5, -4, 4, 5, 6, 7]) / 2.0,
-              random.choice([-7, -6, -5, -4, 4, 5, 6, 7]) / 2.0]
+              random.choice([-7, -6, -5, -4, 4, 5, 6, 7]) / 2.0]  # Sets x and y motion to be a random direction chosen from the values in the list
     paddle = [[5, height / 2 - 20, 5, 40],
-              [width - 10, height / 2 - 20, 5, 40]]
+              [width - 10, height / 2 - 20, 5, 40]]  # Sets the paddles to the middle of the screen on their proper side
 
 reset()
 
 while True:
+    """Main loop that controls the system.
+    Will always delay to run at most fps times per second.
+    The system gets the most recent event from the event queue, often being user input.
+    This event is then processed if it is either a quit command or button press. 
+    Finally the update function is called.
+    """
     clock.tick(fps)
 
     event = pygame.event.poll()
 
+    # Handle the event if it is either a quit or button press
     if event.type == pygame.QUIT:
         pygame.quit()
         sys.exit()
-    elif event.type == KEYDOWN:
-        if event.key == K_r:
+    elif event.type == KEYDOWN:  # If the event type is a button press
+        if event.key == K_r:  # If r key is pressed
             reset()
-        elif event.key == K_q:
-            pygame.event.post(pygame.event.Event(QUIT))
-        elif event.key == K_UP:
-            paddle[rhs][y] -= 20
-        elif event.key == K_DOWN:
-            paddle[rhs][y] += 20
-        elif event.key == K_e:
-            paddle[lhs][y] -= 20
-        elif event.key == K_d:
-            paddle[lhs][y] += 20
+        elif event.key == K_q:  # If q key is pressed
+            pygame.event.post(pygame.event.Event(QUIT))  # Add to event queue the quit event. This will be processed next frame
+        elif event.key == K_UP:  # If up arrow key is pressed
+            paddle[rhs][y] -= 20  # Move right paddle down 20 pixels
+        elif event.key == K_DOWN:  # If up down key is pressed
+            paddle[rhs][y] += 20  # Move right paddle up 20 pixels
+        elif event.key == K_e:  # If e key is pressed
+            paddle[lhs][y] -= 20  # Move left paddle down 20 pixels
+        elif event.key == K_d:  # If d key is pressed
+            paddle[lhs][y] += 20  # Move left paddle up 20 pixels
 
     if not dead:
         update()
     draw()
 
-    pygame.display.flip()
+    pygame.display.flip()  # Update the screen, actually draws things previously specified to be displayed
