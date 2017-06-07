97da39c9bc18d77cfa1179dd46d37c12e7fa0026
Grant Jenks <contact@grantjenks.com>
2017-06-07 10:47:37 -0700

    Try 2 for uplaod

--- freegames/__init__.py
+++ freegames/__init__.py
@@ -1,6 +1,6 @@
 __title__ = 'freegames'
-__version__ = '1.0.1'
-__build__ = 0x010001
+__version__ = '1.0.2'
+__build__ = 0x010002
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2017 Grant Jenks'
