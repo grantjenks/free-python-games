880b8c6d8febcae82b6eac20d3cd0057c29e3a58
Grant Jenks <contact@grantjenks.com>
2017-06-07 12:47:37 -0700

    Update guess.py for Python 3

--- freegames/guess.py
+++ freegames/guess.py
@@ -1,14 +1,12 @@
-"""
-Guess a number within a range game.
-
-Copyright (c) 2014 Grant Jenks
-http://www.grantjenks.com/
+"""Guess a number within a range.
 
 Exercises
+
 1. Change the range to be from 0 to 1,000,000.
 2. Can you still guess the number?
 3. Print the number of guesses made.
 4. Limit the number of guesses to the minimum required.
+
 """
 
 import random
@@ -17,24 +15,21 @@ start = 0
 end = 100
 value = random.randint(start, end)
 
-print "I'm thinking of a number between", start, 'and', end
+print("I'm thinking of a number between", start, 'and', end)
 
 guess = None
 
 while guess != value:
 
     try:
-        guess = int(raw_input('Guess the number: '))
-    except Exception:
-        print 'Whoops! Be sure the number contains only digits.'
+        guess = int(input('Guess the number: '))
+    except:
+        print('Whoops! Be sure the number contains only digits.')
         continue
 
     if guess < value:
-
-        print 'Higher.'
-
+        print('Higher.')
     elif guess > value:
+        print('Lower.')
 
-        print 'Lower.'
-
-print 'Congratulations! You guessed the right answer:', value
+print('Congratulations! You guessed the right answer:', value)
