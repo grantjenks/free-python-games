63bbfd38182f1f1125264f5776bc6bb050d0a073
Grant Jenks <contact@grantjenks.com>
2017-06-07 16:16:26 -0700

    Add exercises to snake.py

--- freegames/snake.py
+++ freegames/snake.py
@@ -1,4 +1,12 @@
-"Classic arcade game Snake."
+"""Snake, classic arcade game.
+
+Excercises
+
+1. How do you make the snake faster or slower?
+2. How can you make the snake go around the edges?
+3. How would you move the food?
+
+"""
 
 from turtle import *
 from random import randrange
