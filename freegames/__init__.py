fecd8db347ad8240370d7aa990cf4f0a7f527e1b
Grant Jenks <contact@grantjenks.com>
2017-06-07 17:10:21 -0700

    Bump version to 1.0.4

--- freegames/__init__.py
+++ freegames/__init__.py
@@ -1,6 +1,6 @@
 __title__ = 'freegames'
-__version__ = '1.0.3'
-__build__ = 0x010003
+__version__ = '1.0.4'
+__build__ = 0x010004
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2017 Grant Jenks'
