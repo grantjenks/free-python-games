cb367852b715b6a56fd0ff468028ab93fa65b736
Grant Jenks <contact@grantjenks.com>
2017-06-09 14:50:19 -0700

    Add note to add arrow key exercise

--- freegames/snake.py
+++ freegames/snake.py
@@ -5,6 +5,7 @@ Excercises
 1. How do you make the snake faster or slower?
 2. How can you make the snake go around the edges?
 3. How would you move the food?
+4. Change the snake to respond to arrow keys.
 
 """
 
