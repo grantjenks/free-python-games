fe4e07ca925917ab59b6a5fe0b3bebc64f75652a
Grant Jenks <contact@grantjenks.com>
2017-06-09 10:13:46 -0700

    Update tron for Python 3 and turtle

--- freegames/tron.py
+++ freegames/tron.py
@@ -1,113 +1,71 @@
-"""
-Tron, the video game.
-
-Copyright (c) 2014 Grant Jenks
-http://www.grantjenks.com/
+"""Tron, classic arcade game.
 
 Exercises
+
 1. Make the tron players faster/slower.
-2. Allow a tron player to run into itself.
-3. Allow the tron players to go around the edge of the screen.
+2. Stop a tron player from running into itself.
+3. Allow the tron player to go around the edge of the screen.
+4. How would you create a computer player?
+
 """
 
-import sys, pygame
-from pygame.locals import *
+from turtle import *
+from freegames import vector
 
-pygame.init()
+p1xy = vector(-100, 0)
+p1aim = vector(4, 0)
+p1body = set()
 
-black, red, blue, white = (0, 0, 0), (255, 0, 0), (0, 0, 255), (255, 255, 255)
-tiles, width = 120, 4
-up, right, down, left = -tiles, 1, tiles, -1
+p2xy = vector(100, 0)
+p2aim = vector(-4, 0)
+p2body = set()
 
-clock = pygame.time.Clock()
-screen = pygame.display.set_mode((tiles * width, tiles * width))
+def square(x, y, side, name):
+    "Draw square at (x, y) with given side length and fill color."
+    up()
+    goto(x, y)
+    down()
+    color(name)
+    begin_fill()
+    for count in range(4):
+        forward(side)
+        left(90)
+    end_fill()
 
-def restart():
-    global board, player1, player2
-    board = [black] * (tiles * tiles)
-    player1 = dict(direction=left,
-                   position=(tiles * 3 / 4) + (tiles * tiles / 2),
-                   dead=False,
-                   color=red)
-    player2 = dict(direction=right,
-                   position=(tiles / 4) + (tiles * tiles / 2),
-                   dead=False,
-                   color=blue)
-    screen.fill(black)
-    pygame.display.flip()
+def inside(head):
+    return -200 < head.x < 200 and -200 < head.y < 200
 
-restart()
+def draw():
+    "Advance players and draw game."
+    p1xy.move(p1aim)
+    p1head = p1xy.copy()
 
-def move(player):
-    curr = player['position']
+    p2xy.move(p2aim)
+    p2head = p2xy.copy()
 
-    curr_x = curr % tiles
-    curr_y = curr / tiles
+    if not inside(p1head) or p1head in p2body:
+        print('Player blue wins!')
+        return
 
-    if player['direction'] == left:
-        curr_x -= 1
-    elif player['direction'] == right:
-        curr_x += 1
-    elif player['direction'] == up:
-        curr_y -= 1
-    elif player['direction'] == down:
-        curr_y += 1
+    if not inside(p2head) or p2head in p1body:
+        print('Player red wins!')
+        return
 
-    curr_x %= tiles
-    curr_y %= tiles
+    p1body.add(p1head)
+    p2body.add(p2head)
 
-    next = curr_x + tiles * curr_y
+    square(p1xy.x, p1xy.y, 3, 'red')
+    square(p2xy.x, p2xy.y, 3, 'blue')
+    update()
+    ontimer(draw, 50)
 
-    if board[next] != black:
-        player['dead'] = True
-    else:
-        board[next] = player['color']
-
-    player['position'] = next
-
-def draw_rect(player):
-    left, top = player['position'] % tiles, player['position'] / tiles
-    return (left * width, top * width, width, width)
-
-while True:
-    clock.tick(36)
-    event = pygame.event.poll()
-
-    if event.type == pygame.QUIT:
-        pygame.quit()
-        sys.exit()
-    elif event.type == KEYDOWN:
-        if event.key == K_UP and player1['direction'] != down:
-            player1['direction'] = up
-        elif event.key == K_RIGHT and player1['direction'] != left:
-            player1['direction'] = right
-        elif event.key == K_DOWN and player1['direction'] != up:
-            player1['direction'] = down
-        elif event.key == K_LEFT and player1['direction'] != right:
-            player1['direction'] = left
-        elif event.key == K_w and player2['direction'] != down:
-            player2['direction'] = up
-        elif event.key == K_d and player2['direction'] != left:
-            player2['direction'] = right
-        elif event.key == K_s and player2['direction'] != up:
-            player2['direction'] = down
-        elif event.key == K_a and player2['direction'] != right:
-            player2['direction'] = left
-        elif event.key == K_r:
-            restart()
-        elif event.key == K_q:
-            pygame.event.post(pygame.event.Event(QUIT))
-
-    if player1['dead'] or player2['dead']:
-        continue
-
-    move(player1)
-    move(player2)
-
-    rect1 = draw_rect(player1)
-    rect2 = draw_rect(player2)
-
-    pygame.draw.rect(screen, player1['color'], rect1)
-    pygame.draw.rect(screen, player2['color'], rect2)
-
-    pygame.display.update((rect1, rect2))
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+listen()
+onkey(lambda: p1aim.rotate(90), 'a')
+onkey(lambda: p1aim.rotate(-90), 'd')
+onkey(lambda: p2aim.rotate(90), 'j')
+onkey(lambda: p2aim.rotate(-90), 'l')
+draw()
+done()
