063d3b55178d1f5f26417a662d0267b35fbbbf14
Grant Jenks <contact@grantjenks.com>
2017-06-11 14:18:48 -0700

    Add exercise ideas to pong

--- freegames/pong.py
+++ freegames/pong.py
@@ -7,6 +7,8 @@ Exercises
 3. Change the speed of the ball.
 4. Change the size of the paddles.
 5. Change how the ball bounces off walls.
+6. How would you add a computer player?
+6. Add a second ball.
 
 """
 
