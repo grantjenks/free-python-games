454cae7e18e0274b527fa3cc1a9d01bc14ce15ac
Grant Jenks <contact@grantjenks.com>
2017-06-12 10:39:21 -0700

    Bump version to 1.2.1

--- freegames/__init__.py
+++ freegames/__init__.py
@@ -1,8 +1,8 @@
 from .utils import floor, path, vector
 
 __title__ = 'freegames'
-__version__ = '1.2.0'
-__build__ = 0x010200
+__version__ = '1.2.1'
+__build__ = 0x010201
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2017 Grant Jenks'
