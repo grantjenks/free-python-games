5995584578b9bc459fce759a72cb3b26c8d962d8
Grant Jenks <contact@grantjenks.com>
2017-06-13 16:54:15 -0700

    Simplify guess

--- freegames/guess.py
+++ freegames/guess.py
@@ -9,27 +9,29 @@ Exercises
 
 """
 
-import random
+from random import randint
 
 start = 0
 end = 100
-value = random.randint(start, end)
+value = randint(start, end)
 
-print("I'm thinking of a number between", start, 'and', end)
+print("I'm thinking of a number between", start, "and", end)
 
 guess = None
 
 while guess != value:
 
+    text = input("Guess the number: ")
+
     try:
-        guess = int(input('Guess the number: '))
+        guess = int(text)
     except:
-        print('Whoops! Be sure the number contains only digits.')
+        print("Whoops! Be sure the number contains only digits.")
         continue
 
     if guess < value:
-        print('Higher.')
+        print("Higher.")
     elif guess > value:
-        print('Lower.')
+        print("Lower.")
 
-print('Congratulations! You guessed the right answer:', value)
+print("Congratulations! You guessed the right answer:", value)
