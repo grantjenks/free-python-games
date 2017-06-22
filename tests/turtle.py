f467befbe46d982242a9b2992da149cb9f28abe1
Grant Jenks <contact@grantjenks.com>
2017-06-21 23:00:43 -0700

    Add mock turtle module for testing

---
+++ tests/turtle.py
@@ -0,0 +1,27 @@
+"""Mock turtle module.
+
+"""
+
+def goto(x, y):
+    pass
+
+def up():
+    pass
+
+def down():
+    pass
+
+def color(name):
+    pass
+
+def begin_fill():
+    pass
+
+def end_fill():
+    pass
+
+def forward(steps):
+    pass
+
+def left(degrees):
+    pass
