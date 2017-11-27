08bf04c196531603eea5a1b245304c098f1d435f
Grant Jenks <contact@grantjenks.com>
2017-11-27 12:09:47 -0800

    Add maze game

---
+++ freegames/maze.py
@@ -0,0 +1,46 @@
+"""Maze, moving from one side to another.
+
+Excercises
+
+1. Keep score by counting taps.
+2. Make the maze harder.
+3. Can you always find a path from one side to another?
+
+"""
+
+from turtle import *
+from random import random
+from freegames import line
+
+def draw():
+    "Draw maze."
+    color('black')
+    width(5)
+
+    for x in range(-200, 200, 40):
+        for y in range(-200, 200, 40):
+            if random() > 0.5:
+                line(x, y, x + 40, y + 40)
+            else:
+                line(x, y + 40, x + 40, y)
+
+    update()
+
+def tap(x, y):
+    "Draw line and dot for screen tap."
+    if abs(x) > 198 or abs(y) > 198:
+        up()
+    else:
+        down()
+
+    width(2)
+    color('red')
+    goto(x, y)
+    dot(4)
+
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+draw()
+onscreenclick(tap)
+done()
