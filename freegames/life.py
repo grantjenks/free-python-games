8f0228cad6dd47b793949bcb02babc0491ed836b
Aaron Crosman <acrosman@gmail.com>
2018-10-09 22:43:47 -0400

    Updates to get an working version.
    Including feedback from discussions in https://github.com/grantjenks/free-python-games/issues/3

--- freegames/life.py
+++ freegames/life.py
@@ -9,87 +9,89 @@ Exercises
 2. Make the grid bigger.
 3. Generate some of the example patterns found here:
     https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Examples_of_patterns
-
+4. Step uses two nested loops back to back. Can you find a way to do the work
+    in one pass?
 """
 
-import turtle
+from turtle import *
 from random import choice
 from freegames import square
 
 # Setup grid.
-x = 20
-y = 20
 grid = []
-boxSize = 10
-stepCount = 0
-
-# On/Off values.
-ON = 255
-OFF = 0
-vals = [ON, OFF]
+box_size = 10
 
 
-def randomGrid(x, y):
+def randomize_grid(x, y):
     """Returns a grid of X by Y random values"""
-    return [
-        [choice(vals) for _ in range(x)]
-        for _ in range(y)
-    ]
+
+    # Add two extra rows and columns to create a border around the actual grid.
+    # to ease to process of checking the actual values during each step.
+    for i in range(x+2):
+        grid.append([])
+        for j in range(y+2):
+            if 0 < i < x+1:
+                if 0 < j < y+1:
+                    grid[i].append(choice([0, 1]))
+                else:
+                    grid[i].append(0)
+            else:
+                grid[i].append(0)
 
 
-def step():
+def step(x, y):
     """Update the grid for one step in the simulation."""
 
     # Copy grid since we require 8 neighbors for calculation
     # and we go line by line (and memory is cheap).
-    newGrid = grid.copy()
+    fresh_grid = []
 
-    print("Step")
+    for i in range(x+2):
+        fresh_grid.append([])
+        for j in range(y+2):
+            fresh_grid[i].append(0)
 
-    for i in range(x):
-        for j in range(y):
-            # Compute 8-neighbor sum using toroidal boundary conditions x and y
-            # wrap around so that the simulation takes place on a toroidal
-            # surface.
-            total = int((grid[i][(j-1) % x] + grid[i][(j+1) % y]
-                     + grid[(i-1) % x][j] + grid[(i+1) % x][j]
-                     + grid[(i-1) % x][(j-1) % y] + grid[(i-1) % x][(j+1) % y]
-                     + grid[(i+1) % x][(j-1) % y] + grid[(i+1) % x][(j+1) % y])
-                     / 255)
+            # Compute 8-neighbor sum. Skip the edges of the grid confining the
+            # simulation to a fixed space.
+            if i == 0 or i == x+1 or j == 0 or j == y+1:
+                total = 0
+            else:
+                total = grid[i][(j-1)] + grid[i][(j+1)] \
+                      + grid[(i-1)][j] + grid[(i+1)][j] \
+                      + grid[(i-1)][(j-1)] + grid[(i-1)][(j+1)] \
+                      + grid[(i+1)][(j-1)] + grid[(i+1)][(j+1)]
 
             # Apply Conway's rules.
-            if grid[i][j] == ON:
-                if (total < 2) or (total > 3):
-                    newGrid[i][j] = OFF
+            if grid[i][j] == 1:
+                if 1 < total < 4:
+                    fresh_grid[i][j] = 1
             else:
                 if total == 3:
-                    newGrid[i][j] = ON
+                    fresh_grid[i][j] = 1
 
-    grid[:] = newGrid[:]
+    grid[:] = fresh_grid[:]
 
 
-def draw():
+def redraw_grid():
+    draw_grid(20, 20)
+
+
+def draw_grid(x, y):
     """Update the simulation display and trigger next pass"""
-    step()
-    print("draw")
-    for i in range(x):
-        for j in range(y):
-            if grid[i][j] == ON:
-                square(i * boxSize, j * boxSize, boxSize, 'black')
+    step(x, y)
+    for i in range(x+2):
+        for j in range(y+2):
+            if grid[i][j] == 1:
+                square(i * box_size, j * box_size, box_size, 'green')
             else:
-                square(i * boxSize, j * boxSize, boxSize, 'white')
+                square(i * box_size, j * box_size, box_size, 'red')
 
-    turtle.ontimer(draw, 20)
-
-
-def load():
-    # Generate random starting grid
-    grid[:] = randomGrid(x, y)[:]
+    ontimer(redraw_grid, 500)
 
 
-turtle.setup(x * boxSize, y * boxSize, 0, 0)
-turtle.hideturtle()
-turtle.tracer(False)
-load()
-draw()
-turtle.done()
+setup(600, 600, 0, 0)
+hideturtle()
+tracer(False)
+randomize_grid(20, 20)
+redraw_grid()
+done()
