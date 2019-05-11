168405193cb6457e47931ccf784de647202929ec
Grant Jenks <contact@grantjenks.com>
2019-05-10 17:48:03 -0700

    Simplify minesweeper, fix reveal algorithm, and add exercises.

--- freegames/minesweeper.py
+++ freegames/minesweeper.py
@@ -2,8 +2,9 @@
 
 Exercises
 
-1. Increase the size of the grid.
-2. ???
+1. What does the `seed(0)` function call do?
+2. Change the number of bombs on the grid.
+3. Change the size of the grid.
 
 """
 
@@ -13,19 +14,19 @@ from turtle import *
 
 seed(0)
 bombs = {}
+shown = {}
 counts = {}
-display = {}
 
 
 def initialize():
-    "Initialize `bombs` and `counts` grids."
+    "Initialize `bombs`, `counts`, and `shown` grids."
     for x in range(-250, 250, 50):
         for y in range(-250, 250, 50):
             bombs[x, y] = False
+            shown[x, y] = False
             counts[x, y] = -1
-            display[x, y] = False
 
-    for _ in range(8):
+    for count in range(8):
         x = randrange(-200, 200, 50)
         y = randrange(-200, 200, 50)
         bombs[x, y] = True
@@ -40,18 +41,21 @@ def initialize():
 
 
 def stamp(x, y, text):
+    "Display `text` at coordinates `x` and `y`."
     square(x, y, 50, 'white')
     color('black')
     write(text, font=('Arial', 50, 'normal'))
 
 
 def draw():
+    "Draw the initial board grid."
     for x in range(-200, 200, 50):
         for y in range(-200, 200, 50):
             stamp(x, y, '?')
 
 
 def end():
+    "Draw the bombs as X's on the grid."
     for x in range(-200, 200, 50):
         for y in range(-200, 200, 50):
             if bombs[x, y]:
@@ -59,6 +63,7 @@ def end():
 
 
 def tap(x, y):
+    "Respond to screen click at `x` and `y` coordinates."
     x = floor(x, 50)
     y = floor(y, 50)
 
@@ -71,20 +76,14 @@ def tap(x, y):
     while pairs:
         x, y = pairs.pop()
         stamp(x, y, counts[x, y])
-        display[x, y] = True
+        shown[x, y] = True
 
-        if counts[x, y] != 0:
-            break
-
-        for i in (-50, 0, 50):
-            for j in (-50, 0, 50):
-                if i == j == 0:
-                    continue
-                pair = x + i, y + j
-                if display[pair]:
-                    continue
-                if counts[pair] == 0:
-                    pairs.append(pair)
+        if counts[x, y] == 0:
+            for i in (-50, 0, 50):
+                for j in (-50, 0, 50):
+                    pair = x + i, y + j
+                    if not shown[pair]:
+                        pairs.append(pair)
 
 
 setup(420, 420, 370, 0)
