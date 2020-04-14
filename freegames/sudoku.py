378df63d3a5aa10523760b66badd3cd61369aab9
JackNarowski <jacknarowski@gmail.com>
2020-04-14 15:03:22 -0500

    Workin on generateSudoku

--- freegames/sudoku.py
+++ freegames/sudoku.py
@@ -1,5 +1,30 @@
-
-
+#Common imports used on other projects here for now
 from random import *
 from turtle import *
 from freegames import vector
+
+def draw():
+    #Placeholder for rendering
+    return 0
+
+def generateSudoku():
+    #Make a board
+    board=[
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+           ]
+    #Now we need to randomize the numbers
+    numbers=[1,2,3,4,5,6,7,8,9]
+
+    #Now  we need to insert these back into the board and make sure they go in legal positions
+
+
+    #Return the board
+    return board
