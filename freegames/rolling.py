8b4447b8e54a27bb3454acc9facde2dc8a85cb18
Vishal Naik <59202862+Monsieurvishal@users.noreply.github.com>
2020-05-06 00:19:56 +0530

    create

---
+++ freegames/rolling.py
@@ -0,0 +1,43 @@
+#simulation type for rolling of dice
+#Loop so that pressing space bar rolls again.
+#Add a constant to vary the number of dice that are rolled.
+#Find a way to animate the roll of dice.
+
+import random
+number=random.randint(1,6)
+if number==1:
+    print("|------------|")
+    print("|            |")
+    print("|     1      |")
+    print("|            |")
+    print("|------------|")
+if number==2:
+    print("|------------|")
+    print("|            |")
+    print("|     2      |")
+    print("|            |")
+    print("|------------|")
+if number==3:
+    print("|------------|")
+    print("|            |")
+    print("|     3      |")
+    print("|            |")
+    print("|------------|")
+if number==4:
+    print("|------------|")
+    print("|            |")
+    print("|     4      |")
+    print("|            |")
+    print("|------------|")
+if number==5:
+    print("|------------|")
+    print("|            |")
+    print("|     5      |")
+    print("|            |")
+    print("|------------|")
+if number==6:
+    print("|------------|")
+    print("|            |")
+    print("|     6      |")
+    print("|            |")
+    print("|------------|")
