e180635ecbafe1a0f8c87140c4af0b47d18e4a79
Grant Jenks <grant.jenks@gmail.com>
2021-05-17 21:45:53 -0700

    Update last exercise

--- freegames/snake.py
+++ freegames/snake.py
@@ -5,7 +5,7 @@ Exercises
 1. How do you make the snake faster or slower?
 2. How can you make the snake go around the edges?
 3. How would you move the food?
-4. Change the snake to respond to arrow keys.
+4. Change the snake to respond to mouse clicks.
 
 """
 
