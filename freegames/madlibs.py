c7dff813f925499a794ff76008a1b4447c3c20e5
Grant Jenks <grant.jenks@gmail.com>
2021-05-31 21:59:30 -0700

    Update madlibs game

--- freegames/madlibs.py
+++ freegames/madlibs.py
@@ -1,41 +1,35 @@
-""" Mad Libs: Funny Story Creation Game.
+"""Mad Libs: Funny Story Creation Game
 
 Exercises:
 
-1. Can you create functionality to replace the given story?
-2. Can you load the story and template from a file?
-3. Can you add additional parts of speech?
-4. Can you add flexibility in a loaded story template?
-5. Can you adjust for grammar? (past tense, etc.)
-6. Can you fix the bug that occurs if there is a period after the story?
-7. At what point would you need to encapsulate into a function?
-8. Can you use a function for this?
-9. What problems can occur to the expected output if the program were to 
-    crash mid-run (ex: due to power failure, unexpected computer freeze etc.)
-10. How could you re-engineer this to allow for more robust retention of 
-    the original story and/or the input collected?
+1. How to replace the story?
+2. How load the story and from a file?
+3. How to add additional parts of speech?
+
 """
 
-story_template = "The [adj] [adj] [n] [v] over the [adj] [n]"
-# The quick brown fox jumps over the lazy dog
-
-new_story = ""
-
-word_type_key = {
-    "[adj]": "adjective",
-    "[n]": "noun",
-    "[v]": "verb",
-    "[adv]": "adverb"
+# The quick brown fox jumps over the lazy dog.
+template = 'The |1| |2| |3| |4| over the |5| |6|.'
+parts = {
+    '1': 'adjective',
+    '2': 'adjective',
+    '3': 'noun',
+    '4': 'verb',
+    '5': 'adjective',
+    '6': 'noun',
 }
 
-"Replace word types with user provided words"
-for char_block in story_template.split(' '):
-    if char_block in word_type_key:
-        new_word = input("Please enter a {}: ".format(
-            word_type_key[char_block]))
-        new_story += " {} ".format(new_word)
-    else:
-        new_story += " {} ".format(char_block)
+chunks = []
 
-print("Your new story:")
-print(new_story)
+for chunk in template.split('|'):
+    if chunk in parts:
+        description = parts[chunk]
+        prompt = 'Enter [{}]: '.format(description)
+        word = input(prompt)
+        chunks.append(word)
+    else:
+        chunks.append(chunk)
+
+print('=' * 80)
+story = ''.join(chunks)
+print(story)
