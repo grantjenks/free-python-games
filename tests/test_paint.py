ea8f41bb28339fda400d2abb1315631310bf1a0f
Grant Jenks <grant.jenks@gmail.com>
2021-06-15 14:26:50 -0400

    Test colors

--- tests/test_paint.py
+++ tests/test_paint.py
@@ -10,18 +10,23 @@ sys.modules['turtle'] = sys.modules['mockturtle']
 def test_paint():
     random.seed(0)
     mockturtle.events[:] = [
+        ('key K',),
         ('key l',),
         ('click', 0, 0),
         ('click', 10, 10),
+        ('key W',),
         ('key s',),
         ('click', 20, 20),
         ('click', 30, 30),
+        ('key G',),
         ('key c',),
         ('click', 30, 30),
         ('click', 40, 40),
+        ('key B',),
         ('key r',),
         ('click', 30, 30),
         ('click', 40, 40),
+        ('key R',),
         ('key t',),
         ('click', 30, 30),
         ('click', 40, 40),
