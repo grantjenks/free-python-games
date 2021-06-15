2babbe04a8a4083663026b387534b42a86a6119b
Grant Jenks <grant.jenks@gmail.com>
2021-06-15 14:26:57 -0400

    Test back-and-forth

--- tests/test_pong.py
+++ tests/test_pong.py
@@ -10,7 +10,8 @@ sys.modules['turtle'] = sys.modules['mockturtle']
 def test_pong_1():
     random.seed(0)
     mockturtle.events.clear()
-    mockturtle.events += [('timer',), ('key s',)] * 8
+    mockturtle.events += [('timer',), ('key s',)] * 12
+    mockturtle.events += [('timer',), ('key w',)] * 4
     mockturtle.events += [('timer', True)] * 600
     runpy.run_module('freegames.pong')
 
@@ -18,6 +19,7 @@ def test_pong_1():
 def test_pong_2():
     random.seed(1)
     mockturtle.events.clear()
-    mockturtle.events += [('timer',), ('key k',)] * 12
+    mockturtle.events += [('timer',), ('key k',)] * 18
+    mockturtle.events += [('timer',), ('key i',)] * 6
     mockturtle.events += [('timer', True)] * 600
     runpy.run_module('freegames.pong')
