9fd4af2b3af669da18cbea457efc5de21279764a
Grant Jenks <grant.jenks@gmail.com>
2021-06-15 14:54:51 -0400

    Update copyright to 2021

--- freegames/__init__.py
+++ freegames/__init__.py
@@ -67,4 +67,4 @@ __version__ = '2.3.2'
 __build__ = 0x020302
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
-__copyright__ = '2017-2020, Grant Jenks'
+__copyright__ = '2017-2021, Grant Jenks'
