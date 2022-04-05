d5ecc169c6696d1ec2efe365f9ee14c25b0c2d34
Bamboudorer <lucas.techer@epitech.eu>
2022-04-06 01:10:28 +0400

    Update for test flake8 and isortcheck

--- freegames/illusion.py
+++ freegames/illusion.py
@@ -1,6 +1,5 @@
 """Illusion, just create a small illusion.
 
-
 Exercises :
 
 1. Change the number of line.
@@ -8,40 +7,43 @@ Exercises :
 """
 
 from turtle import *
-from random import randint
 
 from freegames import line, square
 
 square_size = 25
 
-def drawOneLineSquare(start_x, start_y):
+
+def draw_one_line_square(start_x, start_y):
     for i in range(0, 10):
-        place_x = start_x + (i * square_size *2)
+        place_x = start_x + (i * square_size * 2)
         square(place_x, start_y, square_size, "black")
 
-def drawAllLines():
+
+def draw_all_lines():
     base_x = -200
     base_y = 150
-    for i in range(0,11):
+    for i in range(0, 11):
         line(base_x, base_y - i * square_size, base_x + 16 * square_size, base_y - i * square_size)
 
-def drawAllSquare():
-    drawOneLineSquare(-200, 150)
-    drawOneLineSquare(-190, 125)
-    drawOneLineSquare(-180, 100)
-    drawOneLineSquare(-190, 75)
-    drawOneLineSquare(-200, 50)
-    drawOneLineSquare(-190, 25)
-    drawOneLineSquare(-180, 0)
-    drawOneLineSquare(-190, -25)
-    drawOneLineSquare(-200, -50)
-    drawOneLineSquare(-190, -75)
-    drawOneLineSquare(-180, -100)
+
+def draw_all_square():
+    draw_one_line_square(-200, 150)
+    draw_one_line_square(-190, 125)
+    draw_one_line_square(-180, 100)
+    draw_one_line_square(-190, 75)
+    draw_one_line_square(-200, 50)
+    draw_one_line_square(-190, 25)
+    draw_one_line_square(-180, 0)
+    draw_one_line_square(-190, -25)
+    draw_one_line_square(-200, -50)
+    draw_one_line_square(-190, -75)
+    draw_one_line_square(-180, -100)
+
 
 setup(420, 400, 30, 0)
 hideturtle()
 tracer(False)
 listen()
-drawAllLines()
-drawAllSquare()
+draw_all_lines()
+draw_all_square()
 done()
