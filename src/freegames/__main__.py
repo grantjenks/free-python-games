5e142cd95882ffdf821ce83aae9b919fcbe627b9
Grant Jenks <grant.jenks@gmail.com>
2022-12-27 23:06:23 -0800

    Add "freegames" entry-point and "play" subcommand

--- src/freegames/__main__.py
+++ src/freegames/__main__.py
@@ -3,9 +3,7 @@
 
 import argparse
 import os
-
-directory = os.path.dirname(os.path.realpath(__file__))
-contents = os.listdir(directory)
+import runpy
 
 
 def game_file(name):
@@ -17,49 +15,61 @@ def game_file(name):
     )
 
 
-games = sorted(name[:-3] for name in contents if game_file(name))
+def main():
+    directory = os.path.dirname(os.path.realpath(__file__))
+    contents = os.listdir(directory)
+    games = sorted(name[:-3] for name in contents if game_file(name))
 
-parser = argparse.ArgumentParser(
-    prog='freegames',
-    description='Free Python Games',
-    epilog='Copyright 2023 Grant Jenks',
-)
-subparsers = parser.add_subparsers(dest='command', help='sub-command help')
+    parser = argparse.ArgumentParser(
+        prog='freegames',
+        description='Free Python Games',
+        epilog='Copyright 2023 Grant Jenks',
+    )
+    subparsers = parser.add_subparsers(dest='command', help='sub-command help')
+    subparsers.required = True
 
-parser_list = subparsers.add_parser('list', help='list games')
+    parser_list = subparsers.add_parser('list', help='list games')
 
-parser_copy = subparsers.add_parser('copy', help='copy game source code')
-parser_copy.add_argument('game', choices=games, help='game name')
-parser_copy.add_argument(
-    '--force',
-    action='store_true',
-    help='overwrite existing file',
-)
+    parser_play = subparsers.add_parser('play', help='play free Python games')
+    parser_play.add_argument('game', choices=games, help='game name')
 
-parser_show = subparsers.add_parser('show', help='show game source code')
-parser_show.add_argument('game', choices=games, help='game name')
+    parser_show = subparsers.add_parser('show', help='show game source code')
+    parser_show.add_argument('game', choices=games, help='game name')
 
-args = parser.parse_args()
+    parser_copy = subparsers.add_parser('copy', help='copy game source code')
+    parser_copy.add_argument('game', choices=games, help='game name')
+    parser_copy.add_argument(
+        '--force',
+        action='store_true',
+        help='overwrite existing file',
+    )
 
-if args.command == 'list':
-    for game in games:
-        print(game)
-elif args.command == 'copy':
-    filename = args.game + '.py'
+    args = parser.parse_args()
 
-    with open(os.path.join(directory, filename)) as reader:
-        text = reader.read()
-
-    cwd = os.getcwd()
-    path = os.path.join(cwd, filename)
-
-    if args.force or not os.path.exists(path):
-        with open(path, 'w') as writer:
-            writer.write(text)
+    if args.command == 'list':
+        for game in games:
+            print(game)
+    elif args.command == 'play':
+        runpy.run_module('freegames.' + args.game)
+    elif args.command == 'show':
+        with open(os.path.join(directory, args.game + '.py')) as reader:
+            print(reader.read())
     else:
-        print('ERROR: File already exists. Specify --force to overwrite.')
-else:
-    assert args.command == 'show'
+        assert args.command == 'copy'
+        filename = args.game + '.py'
 
-    with open(os.path.join(directory, args.game + '.py')) as reader:
-        print(reader.read())
+        with open(os.path.join(directory, filename)) as reader:
+            text = reader.read()
+
+        cwd = os.getcwd()
+        path = os.path.join(cwd, filename)
+
+        if args.force or not os.path.exists(path):
+            with open(path, 'w') as writer:
+                writer.write(text)
+        else:
+            print('ERROR: File already exists. Specify --force to overwrite.')
+
+
+if __name__ == '__main__':
+    main()
